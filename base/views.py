f5eb240f518747b682114f9b60dde207d3083127
Ceri Perkins <ceri_perkins@hotmail.com>
2025-07-26 11:24:40 +0100

    add code to copyright so it shows current year

--- base/views.py
+++ base/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 from .models import User
+from datetime import datetime
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    # Current year for copyright
+    # today = datetime.now()
+    # this_year = today.year
+
+    return render(request, 'index.html', {
+        # "year": year,
+        # "this_year": this_year,
+    })
 
 
 def stylists(request):
     return render(request, 'stylists.html')
 
+
 def profile_page(request, pk):
     user = User.objects.get(username=pk)
     context = {'user': user}
-    return render(request, 'profile.html', context) 
+    return render(request, 'profile.html', context)
