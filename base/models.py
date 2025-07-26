b2ce5df157ba1d225459e6ac755a1a96d4b5f00b
Ceri Perkins <ceri_perkins@hotmail.com>
2025-07-26 22:37:35 +0100

    altered model, made migrations, collect static ready for heroku deployment

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True, blank=True)
-
+    
     # profilepic = models.ImageField()
 
     USERNAME_FIELD = 'email'
@@ -17,23 +17,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
+
 
 class Service(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     servicelength = models.DurationField(help_text="Duration in HH:MM:SS")
     is_available = models.BooleanField(default=True)
-
+    # link this model to user model (stylist) - ManyToMany as
+    # multiple stylists can offer the same thing
+    stylist = models.ManyToManyField(
+        User, related_name='services')
+    
+    
     class Meta:
         verbose_name = "Service"
         verbose_name_plural = "Services"
 
     def __str__(self):
         return f"{self.name} - £{self.price}"
-    
-
-    
-
-   
