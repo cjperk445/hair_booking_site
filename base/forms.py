9e67f7d6c7bb361141d22f0660127c0f3cf25bb2
Ceri Perkins <ceri_perkins@hotmail.com>
2025-07-26 21:04:54 +0100

    correct issue with e-mail not being required to sign up and added more style

--- base/forms.py
+++ base/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from allauth.account.forms import SignupForm
+from django.utils.translation import gettext
 
 
 class CustomSignupForm(SignupForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-
-    def signup(self, request, user):
-        user.email = self.cleaned_data['email']
-        user.save()
-        return user
+    first_name = forms.CharField(max_length=30, label="First Name", required=True)
+    last_name = forms.CharField(max_length=30, label="Last Name", required=True)
+      
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields["email"].label = gettext("Email")
