10a206dc545b3d9c8fb534b3d910d2c4fdbea77d
Ceri Perkins <ceri_perkins@hotmail.com>
2025-08-18 20:38:53 +0100

    add clean functionality to the form in booking form, including checking the time and date are when the salon is opening

--- base/forms.py
+++ base/forms.py
@@ -35,5 +35,84 @@ class BookingForm(forms.ModelForm):
         # This will only show the stylists, if they;re labelled as Staff
 
         self.fields['stylist'].queryset = User.objects.filter(is_staff=True)
+
         # the below should only show what the stylist offers
+
         self.fields['service'].queryset = Service.objects.all()
+
+        #widget used to put dropdown calendar in form
+
+        self.fields['date'].widget = forms.DateInput(attrs={
+            'type': 'date',
+            'min': datetime.date.today().isoformat(),  # disables past dates
+            'id': 'id_date',
+            'class': 'form-control'
+        })
+
+        # Add time picker widget
+        self.fields['time'].widget = forms.TimeInput(attrs={
+            'type': 'time',
+            'id': 'id_time',
+            'class': 'form-control',
+            '''
+            Limit the time choices to every fifteen mins, i.e on the hour 
+            quarter past, half past and quarter too. The time is represented
+            in seconds 900 seconds = fifteen minutes
+            '''
+            'step':900,
+        })
+
+    '''
+    The Below Clean function is used to ensure all the information put in the
+    booking form matches the paramaters it should.
+    The function will check for correct date selection (if the salon is open)
+    and correct opening times.
+    Also There is a check that the chosen stylist offers the chosen
+    service
+    '''
+
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
+        stylist = cleaned_data.get('stylist')
+        service = cleaned_data.get('service')
+
+        if not date or not time:
+            return cleaned_data
+
+        # This will map weekday numbers to names
+        weekday = date.weekday()  # Monday = 0, Sunday = 6
+
+        # This defines the hours the salon is open
+        salon_hours = {
+            1: (datetime.time(9, 0), datetime.time(14, 30)),  # Tuesday
+            2: (datetime.time(9, 0), datetime.time(19, 0)),   # Wednesday
+            3: (datetime.time(9, 0), datetime.time(19, 0)),   # Thursday
+            4: (datetime.time(9, 0), datetime.time(18, 0)),   # Friday
+            5: (datetime.time(9, 0), datetime.time(17, 0)),   # Saturday
+        }
+
+        # Date Check - is the salon open?
+
+        if weekday not in salon_hours:
+            self.add_error('date', "Sorry, the salon is closed on that day.")
+            return cleaned_data
+        
+        # time check - is the salon open during that time?
+
+        open_time, close_time = salon_hours[weekday]
+        if time < open_time or time > close_time:
+            self.add_error('time', f"We're open from {open_time.strftime('%H:%M')} to {close_time.strftime('%H:%M')} on that day. Please choose a time within that range.")
+
+        # Stylist service check
+
+        if stylist and service:
+            if not stylist.services.filter(id=service.id).exists():
+                self.add_error('service', f"Sorry {stylist}, doesn't offer that service. Please Choose another service")
+
+        return cleaned_data
+
+
+
+
